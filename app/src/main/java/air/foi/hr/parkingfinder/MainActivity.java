6bba696a714f26494c59e8bfc3ffd9ae336684a6
izavrski <ivana_zavrski@hotmail.com>
2015-08-12 00:18:42 +0200

    Implemented login  and registration of users

--- app/src/main/java/air/foi/hr/parkingfinder/MainActivity.java
+++ app/src/main/java/air/foi/hr/parkingfinder/MainActivity.java
@@ -1,16 +1,74 @@
 package air.foi.hr.parkingfinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnLogout;
+    EditText etName, etLicensePlate, etUsername, etBalance;
+    UserLocalStore userLocalStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etName = (EditText)findViewById(R.id.etName);
+        etLicensePlate = (EditText)findViewById(R.id.etLicensePlate);
+        etUsername = (EditText)findViewById(R.id.etUsername);
+        etBalance = (EditText)findViewById(R.id.etBalance);
+
+        btnLogout = (Button)findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(this);
+
+        userLocalStore = new UserLocalStore(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (authenticate()==true) {
+            displayUserDetails();
+        }else {
+            startActivity(new Intent(this, Login.class));
+        }
+    }
+
+    private boolean authenticate(){
+        return userLocalStore.getUserLoggedIn();
+    }
+
+    private void displayUserDetails(){
+        User user = userLocalStore.getLoggedInUser();
+
+        etUsername.setText(user.username);
+        etName.setText(user.name);
+        etLicensePlate.setText(user.licensePlate);
+        etBalance.setText(user.balance + "");
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.btnLogout:
+
+                userLocalStore.clearUserData();
+                userLocalStore.setUserLoggedIn(false);
+
+
+                startActivity(new Intent(this, Login.class));
+                break;
+        }
+
     }
 
     @Override
