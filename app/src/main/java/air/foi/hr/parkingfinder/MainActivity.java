029739755862d0a70e57dfa8ac7f568f13b8546f
izavrski <ivana_zavrski@hotmail.com>
2015-08-14 19:55:29 +0200

    Added Navigation Drawer User Details are shown in Navigation Drawer instead in MainActivity

--- app/src/main/java/air/foi/hr/parkingfinder/MainActivity.java
+++ app/src/main/java/air/foi/hr/parkingfinder/MainActivity.java
@@ -1,42 +1,102 @@
 package air.foi.hr.parkingfinder;
 
+import android.content.ClipData;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button btnLogout;
-    EditText etName, etLicensePlate, etUsername, etBalance;
     UserLocalStore userLocalStore;
+
+    private ListView mDrawerList;
+    private DrawerLayout mDrawerLayout;
+    private ArrayAdapter<String> mAdapter;
+
+    private ActionBarDrawerToggle mDrawerToggle;
+    private String mActivityTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etName = (EditText)findViewById(R.id.etName);
-        etLicensePlate = (EditText)findViewById(R.id.etLicensePlate);
-        etUsername = (EditText)findViewById(R.id.etUsername);
-        etBalance = (EditText)findViewById(R.id.etBalance);
-
-        btnLogout = (Button)findViewById(R.id.btnLogout);
-
-        btnLogout.setOnClickListener(this);
-
         userLocalStore = new UserLocalStore(this);
+
+        mDrawerList = (ListView)findViewById(R.id.navList);
+
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mActivityTitle = getTitle().toString();
+
+        setupDrawer();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+    }
+
+    private void addDrawerItems() {
+        User user = userLocalStore.getLoggedInUser();
+
+        String[] userDetailsArray = { "Username: ",user.username, "Name:", user.name,"License plate:", user.licensePlate, "Balance:", user.balance+ "", "Logout" };
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, userDetailsArray);
+        mDrawerList.setAdapter(mAdapter);
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedFromList = mDrawerList.getItemAtPosition(position).toString();
+
+                if(selectedFromList.equals("Logout")) {
+                    userLocalStore.clearUserData();
+                    userLocalStore.setUserLoggedIn(false);
+                    startActivity(new Intent(MainActivity.this, Login.class));
+                }
+
+                Toast.makeText(MainActivity.this, selectedFromList, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("User details");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         if (authenticate()==true) {
-            displayUserDetails();
+            addDrawerItems();
         }else {
             startActivity(new Intent(this, Login.class));
         }
@@ -46,29 +106,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return userLocalStore.getUserLoggedIn();
     }
 
-    private void displayUserDetails(){
-        User user = userLocalStore.getLoggedInUser();
-
-        etUsername.setText(user.username);
-        etName.setText(user.name);
-        etLicensePlate.setText(user.licensePlate);
-        etBalance.setText(user.balance + "");
-    }
 
     @Override
     public void onClick(View v) {
 
-        switch (v.getId()){
-            case R.id.btnLogout:
-
-                userLocalStore.clearUserData();
-                userLocalStore.setUserLoggedIn(false);
+    }
 
 
-                startActivity(new Intent(this, Login.class));
-                break;
-        }
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
     @Override
@@ -87,6 +142,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            return true;
+        }else if (id == R.id.exit_the_app){
+            finish();
+            return true;
+        }
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
 
